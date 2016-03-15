bc239c3b5b3924a3bd3c683a67c65cb4e6ada298
borjagodoygago <bgodoyga@alumnos.unex.es>
2016-03-15 20:00:48 +0100

    change naiming

--- demo/model/Grupo_model.js
+++
@@ -1,8 +0,0 @@
-var mongoose= require('mongoose'),
-Schema= mongoose.Schema;
-var grupo= new Schema({
-   "nombre" : String,
-   "rol" : String,
-   "leader" : String
-});
-module.exports= mongoose.model('Grupo',grupo);
